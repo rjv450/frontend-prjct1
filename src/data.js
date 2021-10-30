12d803f4bb6a50a268c877ea8acbe46465d15afe
rjv450 <rijovarghese450@gmail.com>
2021-10-30 20:26:01 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+           id: 1,
+           img: "https://i.ibb.co/kgVZHXt/ei-1634066454082-removebg-preview-1.png",
+           title:"SUMMER SALE",
+           desc:"DON'T COMPROMISE ON STYLE! GET FLAT 20% OFF FOR NEW ARRIVALS",
+           bg:"f5fafd", 
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/wLgc3zL/david-beckham-3-removebg-preview.png",
+        title:"AUTHUMN COLLECTION",
+        desc:"DON'T COMPROMISE ON STYLE! GET FLAT 20% OFF FOR NEW ARRIVALS",
+        bg:"fcf1ed", 
+ },
+ {
+    id: 3,
+    img: "https://i.ibb.co/QKWBVwH/5-56570-beautiful-girl-jacket-blogger-fashion-winter-hd-wallpaper-removebg-preview.png",
+    title:"WINTER COLLECTION",
+    desc:"DON'T COMPROMISE ON STYLE! GET FLAT 20% OFF FOR NEW ARRIVALS",
+    bg:"fcf1ed", 
+},
+];
+
+export const categories = [
+{
+    id:1,
+    img:"https://images.pexels.com/photos/9940432/pexels-photo-9940432.jpeg?cs=srgb&dl=pexels-%D0%B5%D0%BA%D0%B0%D1%82%D0%B5%D1%80%D0%B8%D0%BD%D0%B0-%D1%88%D1%83%D0%BC%D1%81%D0%BA%D0%B8%D1%85-9940432.jpg&fm=jpg",
+    title:"SHIRT STYLE!"
+},
+{
+    id:2,
+    img:"https://images.pexels.com/photos/6599774/pexels-photo-6599774.jpeg?cs=srgb&dl=pexels-thirdman-6599774.jpg&fm=jpg",
+    title:"LOUNGEWEAR LOVE!"
+},
+{
+    id:3,
+    img:"https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg?cs=srgb&dl=pexels-marlene-lepp%C3%A4nen-1183266.jpg&fm=jpg",
+    title:"LIGHT JACKET!"
+},
+];
+
+export const popularProducts = [
+    {
+      id:1,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+      id:2,
+      img:"https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+    },
+    {
+      id:3,
+      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+      id:4,
+      img:"https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+    },
+    {
+      id:5,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:6,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+      id:7,
+      img:"https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+    },
+    {
+      id:8,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+  ]
